5921d1918186fcfa790ffdd6d3c02c99d2e4a286
Michael Lampe <lampewebdev@gmail.com>
2023-09-11 09:05:45 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use clap::{Args, Parser, Subcommand};
+use clap::{Args, Command, Parser, Subcommand};
 use std::{
-    env::{self, var_os},
+    env::{self},
     ffi::OsString,
     fs::{self, remove_dir_all},
-    io::Write,
     path::Path,
-    process::Command,
+    process::{self},
 };
 
 #[derive(Parser)]
@@ -17,7 +16,7 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    /// Init nb
+    /// Initalize rnb
     Init(InitArgs),
     /// Add note to your notebook
     Add(AddArgs),
@@ -45,90 +44,85 @@ struct ResetArgs {}
 
 fn main() {
     let cli = Cli::parse();
-
     match &cli.command {
         Commands::Init(_) => {
-            let working_dir_string = get_working_dir_path();
-            let path = Path::new(&working_dir_string);
-            init(path);
+            let home_dir_path = get_home_path();
+            init(&home_dir_path);
         }
         Commands::Add(add_args) => {
             println!("{:?}", add_args.id);
             let editor_path = get_editor_path();
-            Command::new(editor_path)
-                .arg("absolute/path/to/mytestfile.txt")
-                .status()
-                .expect("Something went wrong.");
-            // Command::new("$EDITOR")
-            //     .status()
-            //     .expect("ls command failed to start");
-            std::process::exit(0);
+            println!("{:?}", editor_path);
+            process::exit(0);
         }
         Commands::Edit(id) => {
             println!("'myapp add' was used, name is: {:?}", id.id)
         }
         Commands::Reset(_) => {
-            let working_dir_string = get_working_dir_path();
-            let path = Path::new(&working_dir_string);
-            reset(path);
-            init(path);
+            let home_dir_path = get_home_path();
+            reset(&home_dir_path);
+            init(&home_dir_path);
         }
     }
 }
 
-static DEFAULT_VALUE_DIR: &str = "~/.rnb";
+static DEFAULT_VALUE_DIR: &str = ".rnb";
+static DEFAULT_RC_FILE: &str = ".rnbrc";
 
-fn get_working_dir_path() -> OsString {
-    match env::var_os("RNB_DIR") {
-        Some(rnb_dir) => rnb_dir,
-        None => {
-            let mut default_dir = OsString::new();
-            default_dir.push(DEFAULT_VALUE_DIR);
-            default_dir
-        }
-    }
-}
+fn reset(home_dir_path: &OsString) {
+    let working_dir_path = Path::new(&home_dir_path).join(DEFAULT_VALUE_DIR);
+    let rc_path = Path::new(&home_dir_path).join(DEFAULT_RC_FILE);
 
-fn reset(path: &Path) {
-    let owned_path = path.to_owned();
-    let copy_path = owned_path.to_str().unwrap();
-    let path_string: String = if copy_path.starts_with('~') {
-        tilde_expand(copy_path)
-    } else {
-        path.to_owned().to_str().unwrap().to_string()
+    match remove_dir_all(&working_dir_path) {
+        Ok(_) => println!(
+            "The rnb folder under {:?} has been deleted",
+            &working_dir_path
+        ),
+        Err(err) => println!("Error while reseting, {:?}", err),
     };
-    let path = Path::new(&path_string);
-
-    match remove_dir_all(path) {
-        Ok(_) => println!("The rnb folder under {:?} has been deleted", path),
+    match fs::remove_file(&rc_path) {
+        Ok(_) => println!("The rnb rc file under {:?} has been deleted", &rc_path),
         Err(err) => println!("Error while reseting, {:?}", err),
     };
 }
 
-fn init(path: &Path) {
-    match path.exists() {
-        true => {
-            println!("The path already exists {:?}", path.to_str().unwrap());
+fn init(home_dir_path: &OsString) {
+    let working_dir_path = Path::new(&home_dir_path).join(DEFAULT_VALUE_DIR);
+    let rc_path = Path::new(&home_dir_path).join(DEFAULT_RC_FILE);
+
+    if working_dir_path.exists() {
+        eprintln!("working dir already excists");
+        process::exit(1)
+    };
+    if rc_path.exists() {
+        eprintln!(".rnbrc file already excists");
+        process::exit(1)
+    };
+    match std::fs::create_dir_all(&working_dir_path) {
+        Ok(_) => println!("Created working dir {:?}", &working_dir_path),
+        Err(err) => {
+            eprintln!("{:?}", err);
+            process::exit(1)
         }
-        false => {
-            println!("Initing rnb...{:?}", path);
-            match fs::create_dir_all(path) {
-                Ok(_) => println!("Done initailzing rnb"),
-                Err(err) => println!("Error while inidtilzint, {:?}", err),
-            };
-            let _ = fs::create_dir_all(path.join("home"));
-            let mut current_file = fs::File::create(path.join(".current")).unwrap();
-            let _ = current_file.write_all("home\n".as_bytes());
-            let home_path_string = get_home_path();
-            let home_path = Path::new(&home_path_string);
-            let mut rc_file = fs::File::create(home_path.join(".rnbrc")).unwrap();
-            let _ = rc_file.write_all("".as_bytes());
+    };
+    match std::fs::File::create(&rc_path) {
+        Ok(_) => println!("Created rc file {:?}", &rc_path),
+        Err(err) => {
+            eprintln!("{:?}", err);
+            process::exit(1)
         }
     };
 }
 
-fn get_editor_path() -> OsString {
-    env::var_os("EDITOR").unwrap()
+fn get_editor_path() -> Option<OsString> {
+    env::var_os("EDITOR").and_then(|e| {
+        if e.is_empty() {
+            eprintln!("Can Not find Editor");
+            process::exit(1)
+        } else {
+            Some(e)
+        }
+    })
 }
 
 // fn get_current_notebook() {
@@ -139,22 +133,25 @@ fn get_editor_path() -> OsString {
 //     let home_path = get_home_path();
 // }
 
-fn get_path_string(path: &String) -> String {
-    let path_string: String = if path.starts_with('~') {
-        tilde_expand(path)
-    } else {
-        path.to_owned()
-    };
-    path_string
+// fn get_path_string(path: &String) -> String {
+//     let path_string: String = if path.starts_with('~') {
+//         tilde_expand(path)
+//     } else {
+//         path.to_owned()
+//     };
+//     path_string
+// }
+
+fn get_home_path() -> OsString {
+    env::var_os("HOME").unwrap_or_else(|| {
+        eprintln!("Could not determine home dir");
+        process::exit(1);
+    })
 }
 
-fn get_home_path() -> String {
-    var_os("HOME").unwrap().into_string().unwrap()
-}
-
-fn tilde_expand(path: &str) -> String {
-    let mut owned_path = path.to_owned();
-    owned_path.remove(0);
-    let home_path: String = var_os("HOME").unwrap().into_string().unwrap();
-    home_path + &owned_path
-}
+// fn tilde_expand(path: &str) -> String {
+//     let mut owned_path = path.to_owned();
+//     owned_path.remove(0);
+//     let home_path: String = var_os("HOME").unwrap().into_string().unwrap();
+//     home_path + &owned_path
+// }
